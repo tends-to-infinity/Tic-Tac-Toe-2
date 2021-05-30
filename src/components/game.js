fb36b55de0da5c578ab85d393d7e9beae8486535
DESH DEEPAK <deshdeepak3335@gmail.com>
2021-05-31 00:41:37 +0530

    Update game.js

--- src/components/game.js
+++ src/components/game.js
@@ -4,6 +4,8 @@ import Board from "./board";
 
 const Game = () => {
 
+  const [ player, setPlayer ] = useState("");
+  const [ choosePlayer, setChoosePlayer ] = useState(true);
   const [ historyOfMoves, setHistoryOfMoves ] = useState([ null, null, null, null, null, null, null, null, null ]);
   const [ stepNumber, setStepNumber ] = useState(-1);
   const [ showHistory, setShowHistory ] = useState(false);
@@ -14,15 +16,15 @@ const Game = () => {
 
     for (let i = 0; i <= stepNumber; i++) {
       i % 2 !== 0
-        ? (boardStatus[ historyOfMoves[ i ] ] = "O")
-        : (boardStatus[ historyOfMoves[ i ] ] = "X");
+        ? (boardStatus[ historyOfMoves[ i ] ] = player === "X" ? "O" : "X")
+        : (boardStatus[ historyOfMoves[ i ] ] = player);
     }
 
     return boardStatus;
   };
 
-  const winner = get_winner(getStatusOfBoard());
-  const player_turn = stepNumber % 2 ? "X" : "O";
+  const winner = get_winner(getStatusOfBoard(), player);
+  const player_turn = (stepNumber % 2 ? "Guest" : "Bot");
 
   const handleClick = (i) => {
     const historyPoint = historyOfMoves.slice(0, stepNumber + 1);
@@ -60,39 +62,66 @@ const Game = () => {
         );
       });
   return (
-    <div className="gaBoardAndHistory">
-      <div className="gaInfoWrapper">
-        <div className="gaDropdown">
-          <span
-            className="gaHistoryHead"
-            onClick={() => setShowHistory((showHistory) => !showHistory)}
-          >
-            Moves
-            </span>
-          {showHistory && (
-            <div className="gaDropdownContent">{renderMoves()}</div>
-          )}
-        </div>
-      </div>
-      <div className="gaBoardAndHeader">
-        <div className="gaAppHead">Tic Tac Toe </div>
-        <Board squares={getStatusOfBoard()} onClick={handleClick} />
-        <div className="gaResultHead">
-          {winner ? (
-            <span className="gaGameStatus">Winner: {winner}</span>
-          ) : stepNumber !== 8 ? (
-            "Next Player: " + player_turn
-          ) : (
-            <span className="gaGameStatus">Game Ends in a Draw</span>
-          )}
-        </div>
-        <div className="gaResetButtonDiv">
-          <button className="gaResetBtn" onClick={() => jumpTo(-1)}>
-            Reset
+    <>
+      {choosePlayer && (
+        <div className="gaPromptBackdrop">
+          <div className="gaPlayerPrompt">
+            <div className="gaPromptHead">Hey Guest!<br /> Make Your Choice to play <br />Tic Tac Toe</div>
+            <button
+              className="gaPlayerBtn"
+              onClick={(e) => {
+                setPlayer(e.target.innerHTML);
+                setChoosePlayer(false);
+              }}
+            >
+              X
             </button>
+            <button
+              className="gaPlayerBtn"
+              onClick={(e) => {
+                setPlayer(e.target.innerHTML);
+                setChoosePlayer(false);
+              }}
+            >
+              O
+            </button>
+          </div>
+        </div>
+      )}
+      <div className="gaBoardAndHistory">
+        <div className="gaInfoWrapper">
+          <div className="gaDropdown">
+            <span
+              className="gaHistoryHead"
+              onClick={() => setShowHistory((showHistory) => !showHistory)}
+            >
+              Moves
+            </span>
+            {showHistory && (
+              <div className="gaDropdownContent">{renderMoves()}</div>
+            )}
+          </div>
+        </div>
+        <div className="gaBoardAndHeader">
+          <div className="gaAppHead">Tic Tac Toe </div>
+          <Board squares={getStatusOfBoard()} onClick={handleClick} />
+          <div className="gaResultHead">
+            {winner ? (
+              <span className="gaGameStatus">Winner: {winner}</span>
+            ) : stepNumber !== 8 ? (
+              "Next Player: " + player_turn
+            ) : (
+              <span className="gaGameStatus">Game Ends in a Draw</span>
+            )}
+          </div>
+          <div className="gaResetButtonDiv">
+            <button className="gaResetBtn" onClick={() => jumpTo(-1)}>
+              Reset
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
