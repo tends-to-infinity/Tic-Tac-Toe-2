f6d53c435c14505a176311bef0bc07acc9939f0f
DESH DEEPAK <deshdeepak3335@gmail.com>
2021-05-31 01:10:31 +0530

    Update winner.js

--- src/components/winner.js
+++ src/components/winner.js
@@ -12,8 +12,6 @@ export function get_winner(squares, player) {
   for (let i = 0; i < lines.length; i++) {
     const [ a, b, c ] = lines[ i ];
     if (squares[ a ] != null && squares[ a ] === squares[ b ] && squares[ a ] === squares[ c ]) {
-      console.log(squares[ a ]);
-      console.log(player);
       return (squares[ a ] === player ? "Guest" : "Bot");
     }
   }
